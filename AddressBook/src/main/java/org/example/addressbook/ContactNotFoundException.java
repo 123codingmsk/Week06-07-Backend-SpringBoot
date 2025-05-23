2933358ed86873a9abc0b244fce1000f8aab5803
Manoj <90825079+123codingmsk@users.noreply.github.com>
2025-05-24 00:07:42 +0530

    Delete AddressBook directory

--- AddressBook/src/main/java/org/example/addressbook/ContactNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.example.addressbook;
-
-public class ContactNotFoundException extends RuntimeException{
-    private String message;
-
-    ContactNotFoundException(String message) {
-        super(message);
-    }
-}
